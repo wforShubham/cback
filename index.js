6ad9a64448d1454093df83ba9e109979fd5b713c
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2025-02-13 21:44:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
-const { OAuth2Client } = require("google-auth-library");
 const jwt = require("jsonwebtoken");
 const User = require('./models/User');
 
