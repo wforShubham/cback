e8b4a82547491f1d99b79bde3cde2d6065c154e6
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2025-03-27 18:36:12 +0530

    Update analyzer.js

--- routes/analyzer.js
+++ routes/analyzer.js
@@ -16,7 +16,7 @@ router.post("/analyze-complexity", cn, async (req, res) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "google/gemini-2.0-pro-exp-02-05:free",
+        model: "google/gemini-2.5-pro-exp-03-25:free",
         messages: [
           {
             role: "user",
